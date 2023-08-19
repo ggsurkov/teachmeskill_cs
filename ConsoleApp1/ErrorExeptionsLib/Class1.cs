9a78ed09faca5fbf664b9d91af84736442a3c848
Georgy <ggsurkov@gmail.com>
2023-08-19 18:20:45 +0200

    added first task

---
+++ ConsoleApp1/ErrorExeptionsLib/Class1.cs
@@ -0,0 +1,27 @@
+﻿namespace ErrorExeptionsLib
+{
+    public class ErrorExeptionsService
+    {
+        static string errorMessage = "Wrong insert. Please, enter the number from 1 to 12";
+        public static bool isCorrectIntMonthValue(dynamic value)
+        {
+            if (value == null)
+            {
+                Console.WriteLine(errorMessage);
+                return false;
+            }
+            if (value.GetType() != typeof(int))
+            {
+                Console.WriteLine(errorMessage);
+                return false;
+            }
+            if (value != null && (value < 1 || value > 12))
+            {
+                Console.WriteLine(errorMessage);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
