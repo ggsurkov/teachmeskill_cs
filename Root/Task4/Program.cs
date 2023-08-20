046bdfd17ee889d75190eda35c35c55ed2851e2b
Georgy <ggsurkov@gmail.com>
2023-08-20 13:48:57 +0200

    added fourth task

---
+++ Root/Task4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Xml.Serialization;
+using ErrorExeptionsLib;
+namespace Task4
+{
+    internal sealed class Program
+    {
+        private static void Main()
+        {
+            int inputNumber = Convert.ToInt32(Console.ReadLine());
+            if (inputNumber > -5)
+            {
+                Console.WriteLine("It's warm");
+                return;
+            }
+            if (inputNumber <= -5 && inputNumber > -20)
+            {
+                Console.WriteLine("It's okay");
+                return;
+            }
+            if (inputNumber <= -20)
+            {
+                Console.WriteLine("It's cold");
+                return;
+            }
+        }
+
+    }
+}
+
+
+
+
+
