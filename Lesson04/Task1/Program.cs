6aff0048d0633c64dfa552075e69f23b714318ec
Georgy <ggsurkov@gmail.com>
2023-08-20 14:55:37 +0200

    added find max number from array

---
+++ Lesson04/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Solution
+{
+    static class Task1
+    {
+        private static void Main()
+        {
+            int[] arr1 = { 3, 5, 1, 9, 15, 11, -17 };
+
+            Console.WriteLine(findHighestNumberFromArray(arr1));
+        }
+
+        public static int findHighestNumberFromArray(int[] array) 
+        {
+            int result = array[0];
+            for (int index = 1; index < array.Length; index++)
+            {
+                if (array[index] > result)
+                    result = array[index];
+            }
+            return result;
+        }
+    }
+}
