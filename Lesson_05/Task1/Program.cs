a4d08ba04f457f491145f934c567a5353d7831ea
Georgy <ggsurkov@gmail.com>
2023-08-27 14:34:59 +0200

    finished

--- Lesson_05/Task1/Program.cs
+++ Lesson_05/Task1/Program.cs
@@ -1,8 +1,15 @@
 ﻿using System;
+using System.Linq;
 using System.Text.RegularExpressions;
 
 namespace StringParser
 {
+
+    class WordsTheSameFirstChar
+    {
+        public List<string> words { get; set; }
+        public char letter { get; set; }
+    }
     class Parser
     {
         public static void Main()
@@ -26,7 +33,8 @@ namespace StringParser
             Console.WriteLine("3) To display at the begin sentences with '?', after with '!'");
             Console.WriteLine("4) To display sentences without ','");
             Console.WriteLine("5) To find words that start with the same character");
-            Console.WriteLine("6) Exit");
+            Console.WriteLine("6) Display words that begining and ending by the same char");
+            Console.WriteLine("7) Exit");
             Console.Write("\r\nSelect an option: ");
 
             switch (Console.ReadLine())
@@ -41,10 +49,15 @@ namespace StringParser
                     DisplayQuestionsFirst(line);
                     return true;
                 case "4":
+                    DisplaySentencesWithoutComma(line);
                     return true;
                 case "5":
+                    DisplayWordsWithSameCharacterStarted(line);
                     return true;
                 case "6":
+                    DisplayWordsWithSameCharacterOfStartedAndEnding(line);
+                    return true;
+                case "7":
                     return false;
                 default:
                     return true;
@@ -85,7 +98,7 @@ namespace StringParser
                 .Replace("7", "siete")
                 .Replace("8", "ocho")
                 .Replace("9", "nueve");
-            Console.WriteLine(line);
+            Console.WriteLine($"\r\n Result: {line}");
         }
 
         static void DisplayQuestionsFirst(string line)
@@ -112,6 +125,8 @@ namespace StringParser
                 }
             }
 
+            Console.WriteLine("Result: \r\n");
+
             foreach (var s in questionsSentences)
             {
                 Console.WriteLine(s);
@@ -125,6 +140,80 @@ namespace StringParser
                 Console.WriteLine(s);
             }
 
+        }
+
+        static void DisplaySentencesWithoutComma(string line)
+        {
+            string[] sentences = Regex.Split(line, @"(?<=[\.!\?])\s+");
+
+            sentences = Array.FindAll(sentences, s => !s.Contains(","));
+
+            Console.WriteLine("Result: \r\n");
+
+            foreach (var sentence in sentences) { 
+                Console.WriteLine(sentence);
+            }
+        }
+
+        static void DisplayWordsWithSameCharacterStarted(string line)
+        {
+            string[] words = line.Split(" ");
+            List<char> firstLetters = new List<char> { };
+            List<WordsTheSameFirstChar> sameWords = new List<WordsTheSameFirstChar> { };
+
+            foreach (var word in words) 
+            {
+                if (!firstLetters.Contains(Char.ToLower(word[0])))
+                {
+                    firstLetters.Add(Char.ToLower(word[0]));
+                }
+                
+            }
+
+            for (int i = 0; i < firstLetters.Count; i++)
+            {
+                for (int j = 0; j < words.Count(); j++)
+                {
+                    if (words[j].ToCharArray()[0] == firstLetters[i])
+                    {
+                        var currentSameWord = sameWords.Find(w => w.letter == firstLetters[i]) ?? null;
+                        if (currentSameWord != null)
+                        {
+                            currentSameWord.words.Add(words[j]);
+                        }
+                        else
+                        {
+                            WordsTheSameFirstChar newSameWord = new WordsTheSameFirstChar() {letter = firstLetters[i], words = new List<string>{ words[j] } };
+                            sameWords.Add(newSameWord);
+                        }
+                    }
+                }
+            }
+
+            foreach (var sM in sameWords)
+            {
+                var wordsResult = String.Join(", ", sM.words.ToArray());
+                
+                Console.WriteLine($"\r\n['{sM.letter}']: {wordsResult}");
+            }
+        }
+
+        static void DisplayWordsWithSameCharacterOfStartedAndEnding(string line)
+        {
+            string[] words = line.Split(" ");
+            List<string> specificWords = new List<string>();
+
+            foreach (var word in words)
+            {
+                if (word[0] == word[word.Length - 1])
+                {
+                    specificWords.Add(word);
+                }
+            }
+
+            var resultedWords = String.Join(", ", specificWords.ToArray());
+            Console.WriteLine($"\r\n Result: {resultedWords}");
+ 
         }
     }
 }
